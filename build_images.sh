eca5910a98176b660de86eb1f742dc24efc9ba1b
Hengjiu Kang <hk3120@columbia.edu>
2021-07-31 01:54:21 -0400

    added workflow to build image

--- build_images.sh
+++ build_images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-image_name="khjtony/sensor-hub-buildtools:x86_64"
-docker build -t $image_name -f ./Dockerfile.builder_x86-64 .
+image_name="khjtony/sensor-hub-buildtools:latest"
+docker build -t $image_name -f ./Dockerfile .
 docker push $image_name
